07778c0cacbae31d4c7f213b4bf523fc96d027d7
nezihegokce <nezihegokcekokcan@gmail.com>
2026-03-02 21:52:08 +0300

    Coding Exercise

---
+++ CodingProject/Exercise1.cs
@@ -0,0 +1,46 @@
+public class Exercise1
+{
+    public static void PrintUsersName()
+    {
+        Console.WriteLine("What is your name?");
+        string username = Console.ReadLine();
+        Console.WriteLine($"Hello {username}");
+    }
+
+    public static void PrintAddResult()
+    {
+        Console.WriteLine("Write First Number");
+        int first = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Write second number");
+        int second = Convert.ToInt16(Console.ReadLine());
+        int sum = first+second;
+        Console.WriteLine(sum);
+
+    }
+
+    public static void PrintOperationResult()
+    {
+        Console.WriteLine("Write First Number");
+        int First = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Write Second Number");
+        int Second = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Select an operation");
+        string operation = Console.ReadLine();
+
+        int result = 0;
+        if(operation == "add")
+        {
+            result = First + Second;
+        }
+        else if (operation == "multiply")
+        {
+            result = First * Second;
+        }
+        else if (operation == "subtract")
+        {
+            result = First - Second;
+        }
+        Console.WriteLine($"Answer Is {result}");
+
+    }
+}
